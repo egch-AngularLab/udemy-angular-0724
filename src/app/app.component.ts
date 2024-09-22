57d0ae76d83ed941358e822ea3761661f73a7b59
Enrico Giurin <enricogiurin@gmail.com>
2024-09-22 21:44:14 +0200

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 //decorator 
 @Component({
   selector: 'app-root',
   standalone: true,  //selector
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
