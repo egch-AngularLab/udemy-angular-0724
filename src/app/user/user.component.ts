da125acde6bd3b4f3da00abaf9fd436806044611
Enrico Giurin <enricogiurin@gmail.com>
2024-10-01 02:45:53 +0200

    dynamic css

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,23 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { User } from './user.model';
 
 //this is equivalent, but not mandatory
 //import { type User } from './user.model';
 
-
-
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
   //here using the type
   //@Input({required: true}) user!: User;
 
   //here using the interface
-  @Input({required: true}) user!: User;
-
-
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
 
   //instead of this we use the output function
   //I can specify the type with generics
@@ -30,13 +26,11 @@ export class UserComponent {
   //we need to specify the type produced by output event, with generics
   //select = output<string>();
 
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
 
- get imagePath(){
-   return 'assets/users/' + this.user.avatar;
- }
- 
- onSelectUser() {
-  this.select.emit(this.user.id);
- }
-
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
