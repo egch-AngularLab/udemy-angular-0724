ac9f3120ce4160df9188ea2667721bea01c17612
Enrico Giurin <enricogiurin@gmail.com>
2024-10-18 01:04:09 +0200

    migrate all components to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 
 //this is the conventional name
 @NgModule({
-    //declarations will be used for modules components
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    //imports will be used for standalone component
-    imports: [BrowserModule ,HeaderComponent, UserComponent, TasksComponent]
+  //declarations will be used for modules components
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TaskComponent,
+    NewTaskComponent,
+    TasksComponent,
+  ],
+  bootstrap: [AppComponent],
+  //imports will be used for standalone component
+  imports: [BrowserModule, FormsModule]
 })
 export class AppModule {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 //this is equivalent, but not mandatory
 //import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
-  standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
-  imports: [CardComponent],
+  styleUrl: './user.component.css'
 })
 export class UserComponent {
   //here using the type
